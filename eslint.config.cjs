eb58e36dc8774e06f7bd3c133f63619ed8aebd72
Bortnik <slenderball2020@gmail.com>
2025-11-02 18:13:29 +0200

    chore: project scaffolding (npm, tsconfig, eslint, prettier, husky, commitlint, scripts)

---
+++ eslint.config.cjs
@@ -0,0 +1,27 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-config-prettier');
+
+module.exports = [
+  { ignores: ['**/*.cjs'] }, // ігноруємо конфігураційні файли у CJS
+  js.configs.recommended, // базові правила JS
+  ...tseslint.configs.recommended, // базові правила TS
+  prettier, // відключення конфліктів з Prettier
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn', // Поки що 'warn', на версії 1.0.0 змінимо
+      'no-unused-vars': 'warn', // Попереджати про невикористані змінні
+    },
+  },
+];
